c8f0557bdadf1f97c8cb2b1ba5fe84d492881d3c
Frederic Scherma <frederic.scherma@gmail.com>
2020-09-21 10:52:32 +0200

    UPD: tickbar generation

--- instrument/tickbar.py
+++ instrument/tickbar.py
@@ -16,32 +16,52 @@ class TickBarBase(object):
     """
     Tick-bar base model for an instrument.
     @note Ofr is a synonym for ask.
-
-    @note 11 floats + 1 bool
     """
 
-    __slots__ = '_timestamp', '_last_timestamp', '_volume', '_ended', '_open_bid', '_open_ofr', '_close_bid', '_close_ofr'
+    __slots__ = '_timestamp', '_last_timestamp', '_volume', '_ended', '_open', '_close', '_ticks', \
+        '_pov', '_pov_bid', '_pov_ofr', '_vol_bid', '_vol_ofr', '_avg_size', '_low', '_high', '_dir'
 
-    def __init__(self, timestamp):
+    def __init__(self, timestamp, price):
+        """
+        @param timestamp Opening timestamp in seconds.
+        """
         self._timestamp = timestamp
         self._last_timestamp = timestamp
 
         self._ticks = []  # array of ticks
 
-        self._open_bid = 0.0
-        self._open_ofr = 0.0
+        # initial open/close
+        self._open = price
+        self._close = price
 
-        self._close_bid = 0.0
-        self._close_ofr = 0.0
+        # initial low/high
+        self._low = price
+        self._high = price
 
         # or in tickbar indicators (could use X=Full or X>=VolumeFilter)
-        self._volume = 0  # total volume for any ticks of the bar
+        self._volume = 0.0  # total volume for any ticks of the bar
+
+        self._vol_bid = 0.0
+        self._vol_ofr = 0.0
+
+        self._avg_size = 0.0  # trade average size or aggreged trades average size ( = volume / num_trades)
+        self._num_trades = 0  # num of total trades or aggreged trades ( +1 at each trade)
+
+        self._pov = 0.0
+        self._pov_bid = 0.0
+        self._pov_ofr = 0.0
+
+        self._dir = 1
 
         # volume delta from prev
         # volume change from prev
         # average size
 
         self._ended = True
+
+    #
+    # data
+    #
 
     @property
     def timestamp(self):
@@ -54,3 +74,139 @@ class TickBarBase(object):
     @property
     def ended(self):
         return self._ended
+
+    @property
+    def ticks(self):
+        return self._ticks
+
+    @property
+    def bid_vol(self, price):
+        if price in self._ticks:
+            return self._ticks[price][0]
+
+        return 0.0
+
+    @property
+    def ofr_vol(self, price):
+        if price in self._ticks:
+            return self._ticks[price][1]
+
+        return 0.0
+    
+    @property
+    def ask_vol(self, price):
+        if price in self._ticks:
+            return self._ticks[price][1]
+
+        return 0.0
+
+    @property
+    def open(self):
+        return self._open
+    
+    @property
+    def close(self):
+        return self._close
+
+    @property
+    def abs_height(self):
+        """
+        Height in ticks from open to close, always absolute.
+        """
+        return self._close - self._open if self._close > self._open else self._open - self._close
+
+    @property
+    def height(self):
+        """
+        Height in ticks from open to close, always relative.
+        """
+        return self._close - self._open
+
+    @property
+    def pov(self):
+        """
+        Returns the price where the bid+ask volume is the max.
+        """
+        return self._pov
+
+    @property
+    def pov_bid(self):
+        return self._pov_bid
+    
+    @property
+    def pov_ofr(self):
+        return self._pov_ofr
+
+    @property
+    def direction(self):
+        return self._dir
+
+    #
+    # processing
+    #
+
+    def complete(self):
+        self._ended = True
+
+        self._update_pov()
+
+    #
+    # protected
+    #
+
+    def _update_pov(self):
+        pg = 0.0
+        vg = 0.0
+
+        pb = 0.0
+        vb = 0.0
+
+        po = 0.0
+        vo = 0.0
+
+        for p, vs in self._ticks.items():
+            # max volume, at price...
+            if vs[0]+vs[1] == vg:
+                # only if price is greater
+                if p > pg:
+                    vg = vs[0]+vs[1]
+                    pg = p
+
+            elif vs[0]+vs[1] > vg:
+                vg = vs[0]+vs[1]
+                pg = p
+
+            # similar for bid but for the lower price
+            if vs[0] == vb:
+                # only if price is lower
+                if p < pb:
+                    vb = vs[0]
+                    pb = p
+
+            elif vs[0] > vb:
+                vb = vs[0]
+                pb = p
+
+            # similar for ofr but for the higher price
+            if vs[1] == vo:
+                # only if price is higher
+                if p > po:
+                    vo = vs[1]
+                    po = p
+
+            elif vs[1] > vo:
+                vo = vs[1]
+                po = p
+
+        self._pov = pg  # retains the price of the higher merged volume
+        self._pov_bid = pb  # and for the bid volume, the lower price
+        self._pov_ofr = po  # and for the ofr volume, the higher price
+
+
+class TickBar(object):
+    """
+    Tick-bar instance for an instrument.
+    """
+
+    def __init__(self, timestamp, price):
+        super().__init__(timestamp, price)
--- instrument/tickbargenerator.py
+++ instrument/tickbargenerator.py
@@ -6,24 +6,27 @@
 from datetime import datetime, timedelta
 from common.utils import UTC
 
-# from instrument.instrument import TickBar
+from instrument.instrument import TickBar
 
 
 class TickBarBaseGenerator(object):
 
     __slots__ = '_size', '_last_timestamp', '_last_consumed'
 
-    def __init__(self, size):
+    def __init__(self, size, tick_size=1.0):
         """
         @param size Generated tick bar tick number.
+        @param tick_size Size of the tick in price (default 1.0)
         """
         self._size = size if size > 0 else 1
+        self._tick_size = tick_size
+
         self._last_consumed = 0
         self._last_timestamp = 0.0
 
     def generate_from_ticks(self, from_ticks):
         """
-        Generate as many tick-bar as possible from the array of ticks given in parameters.
+        Generate as many tick-bar as possible from the array of ticks or trades given in parameters.
         """
         to_tickbars = []
         self._last_consumed = 0
@@ -38,6 +41,9 @@ class TickBarBaseGenerator(object):
         return to_tickbars
 
     def update_from_tick(self, tick):
+        """
+        Overrides this method to implements specifics computed tick-bar model from a single tick or trade.
+        """
         pass
 
 
@@ -49,8 +55,72 @@ class TickBarRangeGenerator(TickBarBaseGenerator):
         """
         super().__init(size)
 
+        self._current = None
+
+    def _new_tickbar(self, tick):
+        # complete the current tick-bar
+        self._current._ended = True
+
+        # return the current as last completed
+        last_tickbar = self._current
+
+        # create a new tick-bar
+        self._current = TickBar(tick[0], tick[1])
+
+        return last_tickbar
+
     def update_from_tick(self, tick):
-        pass
+        if tick[0] < self._last_timestamp:
+            return None
+
+        last_tickbar = None
+
+        if self._current is None:
+            last_tickbar = self._new_tickbar(tick)
+
+        elif tick[1] > self._current._high:
+            # is the price extend the size of the tick-bar outside of its allowed range
+            size = int((tick[1] - self._current._low) / self._tick_size) + 1
+
+            if size > self._size:
+                last_tickbar = self._new_tickbar(tick)
+
+        elif tick[1] < self._current._low:
+            # is the price extend the size of the tick-bar outside of its allowed range
+            size = int((self._current._high - tick[1]) / self._tick_size) + 1
+
+            if size > self._size:
+                last_tickbar = self._new_tickbar(tick)
+
+        # one more trade
+        self._num_trades += 1
+
+        # retain the last trade timestamp
+        self._current._last_timestamp = tick[0]
+
+        # last trade price as close price
+        self._current._close = tick[1]
+
+        # cumulative volume per tick-bar
+        self._current._volume += tick[3]
+
+        # cumulative volume at bid or ask
+        if tick[4] < 0:
+            self._current._vol_bid += tick[3]
+
+            if tick[1] in self._current._ticks:
+
+        elif tick[4] > 0:
+            self._current._vol_ofr += tick[3]
+
+        # retains low and high tick prices
+        self._current._low = min(self._current._low, tick[1])
+        self._current._high = max(self._current._high, tick[1])
+
+        # @todo
+
+        # return the last completed bar or None
+        return last_tickbar
 
 
 class TickBarReversalGenerator(TickBarBaseGenerator):
@@ -61,5 +131,77 @@ class TickBarReversalGenerator(TickBarBaseGenerator):
         """
         super().__init(size)
 
+    def _new_tickbar(self, tick):
+        # complete the current tick-bar
+        self._current._ended = True
+
+        # return the current as last completed
+        last_tickbar = self._current
+
+        # create a new tick-bar
+        self._current = TickBar(tick[0], tick[1])
+
+        # opposite direction tick-bar
+        self._current._dir = -last_tickbar._dir
+
+        return last_tickbar
+
     def update_from_tick(self, tick):
-        pass
+        if tick[0] < self._last_timestamp:
+            return None
+
+        last_tickbar = None
+
+        if self._current is None:
+            # complete the current tick-bar
+            self._current._ended = True
+
+            # return the current as last completed
+            last_tickbar = self._current
+
+            # create a new tick-bar
+            self._current = TickBar(tick[0], tick[1])
+
+        elif tick[1] < self._current._open and self._current._dir > 0:
+            # down-tick from open
+            size = int((self._current._open - tick[1]) / self._tick_size) + 1:
+
+            if size > self._size:
+                self._current = TickBar(tick[0], tick[1])
+
+        elif tick[1] > self._current._open and self._current._dir < 0:
+            # up-tick from open
+            size = int((tick[1] - self._current._open) / self._tick_size) + 1:
+
+            if size > self._size:
+                self._current = TickBar(tick[0], tick[1])
+
+        # one more trade
+        self._num_trades += 1
+
+        # retain the last trade timestamp
+        self._current._last_timestamp = tick[0]
+
+        # last trade price as close price
+        self._current._close = tick[1]
+
+        # cumulative volume per tick-bar
+        self._current._volume += tick[3]
+
+        # cumulative volume at bid or ask
+        if tick[4] < 0:
+            self._current._vol_bid += tick[3]
+            
+            if tick[1] in self._current._ticks:
+
+        elif tick[4] > 0:
+            self._current._vol_ofr += tick[3]
+
+        # retains low and high tick prices
+        self._current._low = min(self._current._low, tick[1])
+        self._current._high = max(self._current._high, tick[1])
+
+        # @todo
+
+        # return the last completed bar or None
+        return last_tickbar
