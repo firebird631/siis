45059ef7ba29908a64afe27ba7d3f098cf7f0df4
Frederic Scherma <frederic.scherma@gmail.com>
2022-05-04 16:12:50 +0200

    UPD: typo

--- notifier/android/androidpush.py
+++ notifier/android/androidpush.py
@@ -8,9 +8,10 @@ import requests
 
 FCM_URL = "https://fcm.googleapis.com/fcm/send"
 
+
 def send_to_android(auth_key, channel, title, message, sound="default"):
     """
-    Post a message to SiiS Adnroid application using Firebase.
+    Post a message to SiiS Android application using Firebase.
     """
     payload = {
         "to": channel or "/topics/default",
