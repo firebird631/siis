138eea989c67bbb1f059d13f3999f7276d269c71
Frederic Scherma <frederic.scherma@gmail.com>
2023-08-19 22:43:13 +0200

    ADD: some dukascopy symbols

--- watcher/connector/dukascopy/symbols.py
+++ watcher/connector/dukascopy/symbols.py
@@ -3,22 +3,36 @@
 # @license Copyright (c) 2023 Dream Overflow
 # dukascopy.com symbols and decimals
 
-# @todo complete and precision
+# @see https://www.dukascopy.com/swiss/french/cfd/range-of-markets/
+# remove dot and slash from symbols to have identifier
+# but have to test to determine the decimal place
+
 DUKASCOPY_SYMBOLS_DECIMALS = {
     'USA500IDXUSD': 0.001,
     'USATECHIDXUSD': 0.001,
     'USA30IDXUSD': 0.001,
-    'DEU30IDXEUR': 0.001,
-    'FRAIDX': 0.001,
+    'DEUIDXEUR': 0.001,
+    'FRAIDXEUR': 0.001,
     'XAUUSD': 0.001,
     'AUDNZD': 0.00001,
     'AUDUSD': 0.00001,
     'EURCAD': 0.00001,
     'EURCHF': 0.00001,
     'EURGBP': 0.00001,
-    'EURJPY': 0.00001,
+    'EURJPY': 0.001,
     'EURUSD': 0.00001,
     'GBPUSD': 0.00001,
     'USDCHF': 0.00001,
-    'USDJPY': 0.00001,
+    'USDJPY': 0.001,
+    # @todo complete FOREX, Oil, Silver, Commodities, Equities US,FR,DE...
+    # BRENT.CMD/USD 1.00 brent oil
+    # LIGHT.CMD/USD 1.00 brut oil
+    # GAS.CMD/USD 0.1 gaz naturel
+    # COPPER.CMD/USD 0.1 cuivre
+    # DOLLAR.IDX/USD DXY
+    # VOL.IDX/USD VOLIDXUSD VIX
+    # BUND.TR/EUR BUNDTREUR Euro Bund
+    # UKGILT.TR/GBP UKGILTTRGBP GBP Bund
+    # USTBOND.TR/USD USTBONDTRUSD US Bond
+    # US, FR, GER Equities ...
 }
